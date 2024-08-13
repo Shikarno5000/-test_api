1d251dda96f260171eda57bcc05c3c77ba534999
Андрей Мартынов <a.martynov@matriks.group>
2024-08-13 15:10:14 +0300

    2.2 Формирование первого тестового варианта

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    echo "123";
     return view('welcome');
 });
+
+
+Route::prefix('test')->controller(App\Http\Controllers\TestController::class)->group(function () {
+    Route::get('/', 'index');
+  });
